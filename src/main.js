5c454a0c465d4f239034bae758c89b771541df49
Gernot Raudner <gitkraken@gernotraudner.at>
2018-03-11 23:18:26 +0100

    fixed component reference

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import SummonerSearchResult from './components/summoner/SummonerSearchResult';
 import router from './router';
 
 Vue.config.productionTip = false;
@@ -11,6 +10,6 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
-  components: { App, SummonerSearchResult },
+  components: { App },
   template: '<App/>',
 });
